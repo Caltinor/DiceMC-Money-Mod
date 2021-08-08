f9a9d17e3955f9f97ed9500760143d9f0bafe74f
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-08-08 13:02:29 -0400

    fix API null server issue

--- src/main/java/dicemc/money/MoneyMod.java
+++ src/main/java/dicemc/money/MoneyMod.java
@@ -1,10 +1,12 @@
 package dicemc.money;
 
 import dicemc.money.FTBQuests.FTBQHandler;
+import dicemc.money.api.MoneyManager;
 import dicemc.money.commands.AccountCommandRoot;
 import dicemc.money.commands.AccountCommandTop;
 import dicemc.money.commands.ShopCommandBuilder;
 import dicemc.money.setup.Config;
+import dicemc.money.storage.DatabaseManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -14,11 +16,13 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MoneyMod.MOD_ID)
 public class MoneyMod {
 	public static final String MOD_ID = "dicemcmm";
+	public static DatabaseManager dbm;
 	
 	public MoneyMod() {		
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);		
@@ -37,6 +41,18 @@ public class MoneyMod {
 
 	}
 	
+	@SubscribeEvent 
+	public void onServerStart(FMLServerStartingEvent event ) {
+		MoneyManager.get().setWorld(event.getServer().overworld());
+		System.out.println(event.getServer().getWorldData().toString());
+		if (Config.ENABLE_HISTORY.get()) {
+			String worldname = getWorldName(event.getServer().getWorldData().toString());
+			System.out.println(worldname);
+			String urlIn = event.getServer().getServerDirectory().getAbsolutePath() + "\\saves\\" + worldname +"\\";
+			dbm = new DatabaseManager(worldname, urlIn);
+		}
+	}
+	
 	@SubscribeEvent
 	public void onCommandRegister(RegisterCommandsEvent event) {
 		AccountCommandRoot.register(event.getDispatcher());
@@ -53,5 +69,13 @@ public class MoneyMod {
 		public ResourceLocation key;
 		
 		AcctTypes(ResourceLocation res) {key = res;}
+	}
+	
+	private static String getWorldName(String raw) {
+		int start = raw.indexOf("[")+1;
+		int end = raw.length()-1;
+		start = (start >= 0 && start < end) ? start : 0;
+		end = (end >= 0) ? end : 0;
+		return raw.substring(start, end);
 	}
 }
